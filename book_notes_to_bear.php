c72648a8f1348000833b058d4eaa68e9c4045b80
Fabian Mu <hello@fabian.mu>
2020-06-22 15:03:08 +0200

    syntax

--- book_notes_to_bear.php
+++ book_notes_to_bear.php
@@ -21,15 +21,15 @@
  *  
  * 
  */
- 
+
 // Default file locations for required iBooks data 
-define('BOOKS_DATABASE_DIRECTORY','~/Library/Containers/com.apple.iBooksX/Data/Documents/BKLibrary');
-define('NOTES_DATABASE_DIRECTORY','~/Library/Containers/com.apple.iBooksX/Data/Documents/AEAnnotation');
+define('BOOKS_DATABASE_DIRECTORY', '~/Library/Containers/com.apple.iBooksX/Data/Documents/BKLibrary');
+define('NOTES_DATABASE_DIRECTORY', '~/Library/Containers/com.apple.iBooksX/Data/Documents/AEAnnotation');
 
 // Verify presence of iBooks database
 
-$path = exec('ls '.BOOKS_DATABASE_DIRECTORY."/*.sqlite");
-define('BOOKS_DATABASE_FILE',$path);
+$path = exec('ls ' . BOOKS_DATABASE_DIRECTORY . "/*.sqlite");
+define('BOOKS_DATABASE_FILE', $path);
 // if(!file_exists(BOOKS_DATABASE_DIRECTORY)){
 // 	die("Sorry, couldn't find an iBooks Library on your Mac. Have you put any books in there?\n");
 // }else{
@@ -42,8 +42,8 @@ define('BOOKS_DATABASE_FILE',$path);
 
 
 // // Verify presence of iBooks notes database
-$path = exec('ls '.NOTES_DATABASE_DIRECTORY."/*.sqlite");
-define('NOTES_DATABASE_FILE',$path);
+$path = exec('ls ' . NOTES_DATABASE_DIRECTORY . "/*.sqlite");
+define('NOTES_DATABASE_FILE', $path);
 // if(!file_exists(NOTES_DATABASE_DIRECTORY)){
 // 	die("Sorry, couldn't find any iBooks notes on your Mac. Have you actually taken any notes in iBooks?\n");
 // }else{
@@ -56,62 +56,68 @@ define('NOTES_DATABASE_FILE',$path);
 
 var_dump($argv);
 if (!isset($argv[1])) {
-    die ("You must specify your Bear.app API Token as argument");
+    die("You must specify your Bear.app API Token as argument");
 }
 define('BEAR_APP_TOKEN', $argv[1]);
 
 // Fire up a SQLite parser
-class MyDB extends SQLite3{
-  function __construct($FileName)
-  {
-     $this->open($FileName);
-  }
+class MyDB extends SQLite3
+{
+    function __construct($FileName)
+    {
+        $this->open($FileName);
+    }
 }
 
-function slugify($text) {
-  // replace non letter or digits by -
-  $text = preg_replace('~[^\pL\d]+~u', '-', $text);
+function slugify($text)
+{
+    // replace non letter or digits by -
+    $text = preg_replace('~[^\pL\d]+~u', '-', $text);
 
-  // transliterate
-  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+    // transliterate
+    $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
 
-  // remove unwanted characters
-  $text = preg_replace('~[^-\w]+~', '', $text);
+    // remove unwanted characters
+    $text = preg_replace('~[^-\w]+~', '', $text);
 
-  // trim
-  $text = trim($text, '-');
+    // trim
+    $text = trim($text, '-');
 
-  // remove duplicate -
-  $text = preg_replace('~-+~', '-', $text);
+    // remove duplicate -
+    $text = preg_replace('~-+~', '-', $text);
 
-  // lowercase
-  $text = strtolower($text);
+    // lowercase
+    $text = strtolower($text);
 
-  if (empty($text)) {
-    return 'n-a';
-  }
+    if (empty($text)) {
+        return 'n-a';
+    }
 
-  return $text;
+    return $text;
 }
 
-function getNoteContent($identifier) {
-    exec('xcall.app/Contents/MacOS/xcall -url "bear://x-callback-url/open-note?id='. $identifier . '" > tmp', $output);
+function getNoteContent($identifier)
+{
+    exec('xcall.app/Contents/MacOS/xcall -url "bear://x-callback-url/open-note?id=' . $identifier . '" > tmp', $output);
     $noteContent = file_get_contents('tmp');
     return json_decode($noteContent)->note;
 }
 
-function createNote($title, $slug, $content) {
+function createNote($title, $slug, $content)
+{
     exec('xcall.app/Contents/MacOS/xcall -url "bear://x-callback-url/create?title=Book%3A%20' . rawurlencode($title) . '&text=' . rawurlencode($content) . '" tmp', $output);
     $noteContent = file_get_contents('tmp');
     sleep(1);
     return json_decode($noteContent);
 }
 
-function addNote($identifier, $annotation) {
+function addNote($identifier, $annotation)
+{
     exec('xcall.app/Contents/MacOS/xcall -url "bear://x-callback-url/add-text?id=' . $identifier . '&mode=append&text=' . rawurlencode($annotation) . '" > tmp', $output);
 }
 
-function searchBook($title) {
+function searchBook($title)
+{
     exec('xcall.app/Contents/MacOS/xcall -url "bear://x-callback-url/search?term=Book%3A%20' . rawurlencode($title) . '&show_window=no&token=' . BEAR_APP_TOKEN . '" > tmp', $output, $return_var);
     $searchResult = file_get_contents('tmp');
     $searchResult = json_decode($searchResult);
@@ -128,9 +134,9 @@ function searchBook($title) {
 $books = array();
 $booksdb = new MyDB(BOOKS_DATABASE_FILE);
 
-if(!$booksdb){
-  echo $booksdb->lastErrorMsg();
-} 
+if (!$booksdb) {
+    echo $booksdb->lastErrorMsg();
+}
 
 $res = $booksdb->query("
 			SELECT
@@ -140,21 +146,21 @@ $res = $booksdb->query("
 			FROM ZBKLIBRARYASSET
 			WHERE ZTITLE IS NOT NULL");
 
-while($row = $res->fetchArray(SQLITE3_ASSOC) ){
-	$books[$row['ZASSETID']] = $row;
+while ($row = $res->fetchArray(SQLITE3_ASSOC)) {
+    $books[$row['ZASSETID']] = $row;
 }
 
 $booksdb->close();
 
-if(count($books)==0) die("No books found in your library. Have you added any to iBooks?\n");
+if (count($books) == 0) die("No books found in your library. Have you added any to iBooks?\n");
 
 // Retrieve the notes.
 
 $notesdb = new MyDB(NOTES_DATABASE_FILE);
 
-if(!$notesdb){
-  echo $notesdb->lastErrorMsg();
-} 
+if (!$notesdb) {
+    echo $notesdb->lastErrorMsg();
+}
 
 $notes = array();
 
@@ -171,30 +177,30 @@ $res = $notesdb->query("
 			WHERE ZANNOTATIONSELECTEDTEXT IS NOT NULL
 			ORDER BY ZANNOTATIONASSETID ASC,Created ASC");
 
-while($row = $res->fetchArray(SQLITE3_ASSOC) ){
-	$notes[$row['ZANNOTATIONASSETID']][] = $row;
+while ($row = $res->fetchArray(SQLITE3_ASSOC)) {
+    $notes[$row['ZANNOTATIONASSETID']][] = $row;
 }
 
 $notesdb->close();
 
 
-if(count($notes)==0) die("No notes found in your library. Have you added any to iBooks?\n\nIf you did on other devices than this Mac, make sure to enable iBooks notes/bookmarks syncing on all devices.");
+if (count($notes) == 0) die("No notes found in your library. Have you added any to iBooks?\n\nIf you did on other devices than this Mac, make sure to enable iBooks notes/bookmarks syncing on all devices.");
 0;
 
-foreach($notes as $AssetID => $booknotes){
+foreach ($notes as $AssetID => $booknotes) {
     if (array_key_exists($AssetID, $books)) {
-		$bookTitle = $books[$AssetID]['Title'];
-		$bookAuthor = $books[$AssetID]['Author'];
-	} else {
-		$bookTitle = "Unknown book";
-		$bookAuthor = "Unknown";
+        $bookTitle = $books[$AssetID]['Title'];
+        $bookAuthor = $books[$AssetID]['Author'];
+    } else {
+        $bookTitle = "Unknown book";
+        $bookAuthor = "Unknown";
     }
 
     $slug = slugify($bookTitle);
-    
-    $header = "by $bookAuthor Export date: " . @strftime('%d-%m-%Y %H%:%M',time() ) ."\n\n";
+
+    $header = "by $bookAuthor Export date: " . @strftime('%d-%m-%Y %H%:%M', time()) . "\n\n";
     $header .= "#book/" . $slug . "\n\n";
-    
+
     $bearBook = searchBook($bookTitle);
     $bearBookContent = "";
     if ($bearBook) {
@@ -204,13 +210,13 @@ foreach($notes as $AssetID => $booknotes){
         echo $bookTitle . " is new\n";
     }
 
-	foreach($booknotes as $note){
-		// Skip empty notes
-		if(strlen($note['BroaderText']?$note['BroaderText']:$note['SelectedText'])==0) continue;
-		
+    foreach ($booknotes as $note) {
+        // Skip empty notes
+        if (strlen($note['BroaderText'] ? $note['BroaderText'] : $note['SelectedText']) == 0) continue;
+
         $highlightedText = $note['SelectedText'];
         $annotation = "";
-        
+
         // leverage Bear.app's ::text:: Syntax for marked text
         if (!empty($note['BroaderText']) && $note['BroaderText'] != $note['SelectedText']) {
             $annotation .= str_replace(str_replace("\n", "", $note['SelectedText']), "::{$note['SelectedText']}::", $note['BroaderText']) . "\n";
@@ -220,13 +226,13 @@ foreach($notes as $AssetID => $booknotes){
         } else {
             $annotation .= "::$highlightedText::\n";
         }
-        
-		$annotation .= "Date: " . @strftime('%d-%m-%Y %H%:%M', @strtotime("2001-01-01 +". ((int)$note['Created'])." seconds")) . "\n";
-		if (!empty($note['Note'])) {
-			$annotation .= "> Note: " . $note['Note'] . "\n\n";
-		}
-		if (!empty($note['Chapter'])) {
-			$annotation .= "Chapter: " . $note['Chapter'] . "\n";
+
+        $annotation .= "Date: " . @strftime('%d-%m-%Y %H%:%M', @strtotime("2001-01-01 +" . ((int) $note['Created']) . " seconds")) . "\n";
+        if (!empty($note['Note'])) {
+            $annotation .= "> Note: " . $note['Note'] . "\n\n";
+        }
+        if (!empty($note['Chapter'])) {
+            $annotation .= "Chapter: " . $note['Chapter'] . "\n";
         }
 
         if (!$bearBook) {
